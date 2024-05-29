b45171bdd113b91c55444e3512b0448a70bce31b
Jerrisaurio <brandon.villegas03@gmail.com>
2024-05-29 11:14:23 -0600

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Roboto, Lusitana, Noto_Sans_Javanese, Jacquard_24 } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Jacquard_24({
+    weight: ['400', '400'],
+    subsets: ['latin'],
+})
